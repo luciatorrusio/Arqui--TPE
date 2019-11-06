48d104c1f78b20bf16d7cb28d326293a990f7226
luciatorrusio <lucia.torrusio@gmail.com>
2019-11-06 18:36:21 -0300

    matriz de bloques

---
+++ Userland/SampleCodeModule/Game.c
@@ -0,0 +1,9 @@
+#include "./include/Game.h"
+
+int MIDDLE = SCREEN_WIDTH/2;
+int NBLOQUES = 20; //Cantidad de bloques
+int runGame(void){
+    int bloques[NBLOQUES];
+    int status[]={MIDDLE, ALL_BLOCKS, } //Donde status= {pos_barra(x), matrizDeBloques(si esta en 1, sino en 0), pos de pelota(x,y)}
+
+}

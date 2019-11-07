c465f88e047e8b81c5b9fa266cbfe4a616b24c5d
luciatorrusio <lucia.torrusio@gmail.com>
2019-11-06 21:16:59 -0300

    lo bueno es que con esto aprendi la contrase;a

--- Userland/SampleCodeModule/Game.c
+++ Userland/SampleCodeModule/Game.c
@@ -1,13 +1,13 @@
 #include "./include/Game.h"
 
-int XMIDDLE = SCREEN_WIDTH/2;  //la mitad en x de la panalla
-int YMIDDLE = SCREEN_HEIGHT/2;  //la mitad  en y de la pantalla
-int R_BLOCKS = 4; //Cantidad de filas de bloques
-int C_BLOCKS = 5; //Cantidad de columnas de bloques
-int LIVESi = 3;     //cantidad de vidas al iniciar el juego
-enum ball_direc{LU, U, RU, RD, D, LD}; //Left up, up, right up, right down, down, left down
-bool barHitWall;
-enum walls{LEFT, RIGHT, UPPER, FLOOR};
+int XMIDDLE = SCREEN_WIDTH/2;           //la mitad en x de la panalla
+int YMIDDLE = SCREEN_HEIGHT/2;          //la mitad  en y de la pantalla
+int R_BLOCKS = 4;                       //Cantidad de filas de bloques
+int C_BLOCKS = 5;                       //Cantidad de columnas de bloques
+int LIVESi = 3;                         //cantidad de vidas al iniciar el juego
+enum ball_direc{LU, U, RU, RD, D, LD};  //Left up, up, right up, right down, down, left down
+enum walls{LEFT, RIGHT, UPPER, FLOOR};  //los bordes de la pantalla
+int bar_vel= 1;                         //velocidad de la barra 
 
 int runGame(void){
     
@@ -42,16 +42,16 @@ int runGame(void){
 
  //inicializa el juego(empieza a pintar la pantalla)
 
-int startGame(int pos_bar,int[R_BLOCKS][C_BLOCKS] blocks, int [2] ball_pos, int ball_vel, ball_direc, lives){
+int startGame(int bar_pos,int[R_BLOCKS][C_BLOCKS] blocks, int [2] ball_pos, int ball_vel, ball_direc, lives){
     
     print_ball(ball_pos);
     print_blocks(blocks);
-    print_bar(pos_bar);
+    print_bar(bar_pos);
 
 //barHitWall devuelve un int que representa que pared esta chocando (enum walls)
     if(barHitWall()){
         if (left_arrow_pressed() && Hit()==RIGHT){
-
+            bar_pos=bar_vel;
         }
     }
 

37b4077604d9ac558b3bfd72dffdbf9f6de67e33
luciatorrusio <lucia.torrusio@gmail.com>
2019-11-06 23:09:55 -0300

    a

--- Userland/SampleCodeModule/Game.c
+++ Userland/SampleCodeModule/Game.c
@@ -5,7 +5,7 @@ int YMIDDLE = SCREEN_HEIGHT/2;                      //la mitad  en y de la panta
 int R_BLOCKS = 4;                                   //Cantidad de filas de bloques
 int C_BLOCKS = 5;                                   //Cantidad de columnas de bloques
 enum ballDirec{LU, U, RU, RD, D, LD}ball_direc;     //Left up, up, right up, right down, down, left down
-enum walls{LEFT, RIGHT, UPPER, FLOOR}wall;          //los bordes de la pantalla
+enum walls{NONE = 0, LEFT, RIGHT, UPPER, FLOOR}wall;          //los bordes de la pantalla
 int bar_vel= 1;                                     //velocidad de la barra 
 int LIVESi = 3;                                     //cantidad de vidas al iniciar el juego    
 int lives;                                          //cantidad de vidas que tiene
@@ -42,6 +42,7 @@ int runGame(void){
     
     
     startGame(pos_bar, blocks, ball_pos, ball_vel, ball_direc, lives);      //inicializa el juego(empieza a pintar la pantalla)
+    
     return 0;
 }
 
@@ -49,7 +50,7 @@ int runGame(void){
 
  //inicializa el juego(empieza a pintar la pantalla)
 
-int startGame(int bar_pos,int[R_BLOCKS][C_BLOCKS] blocks, int [2] ball_pos, int ball_vel, int ball_direc, int lives){
+int startGame(int bar_pos,int[R_BLOCKS][C_BLOCKS] blocks, int [2] ball_pos, int ball_vel, ballDirec ball_direc, int lives){
     
     print_ball(ball_pos);
     print_blocks(blocks);
@@ -57,7 +58,7 @@ int startGame(int bar_pos,int[R_BLOCKS][C_BLOCKS] blocks, int [2] ball_pos, int 
 
 //barHitWall devuelve un int que representa que pared esta chocando (enum walls)
     if(left_arrow_pressed()){
-       if(!barHitWall() || barHitWall() == RIGHT){      //FIJARSE QUE TIENE QUE DEVOLVER barHitWall() para que me de false cuando no hitea
+       if(!barHitWall() || barHitWall() == RIGHT){      
              bar_pos  -= bar_vel;                     //la muevo para la izquierda
        }
     }
@@ -66,13 +67,23 @@ int startGame(int bar_pos,int[R_BLOCKS][C_BLOCKS] blocks, int [2] ball_pos, int 
             bar_pos += bar_vel;
         }
     }
-    if(ballHitWall()){
-        if(barHitWall() == FLOOR){
+
+//Para la pelota
+    switch(ballHitWall()){
+        case FLOOR:
             lives -=1; 
             ball_pos[]={XMIDDLE, YMIDDLE};
             pos_bar = XMIDDLE;
-        }
+            break;
+        case LEFT:    
+        case RIGHT:
+            invertDirectionLR();
+            break;
+        case UPPER:
+            invertDirectionU();
+            break;
     }
+    if(ballHitBlock())
 
 }
 
@@ -86,11 +97,14 @@ int startGame(int bar_pos,int[R_BLOCKS][C_BLOCKS] blocks, int [2] ball_pos, int 
 /* funciones a hacer=
     .bool left_arrow_pressed()
     .bool right_arrow_pressed()
-    .int barHitWall()               devuelve el wall, (enum walls)
-    .ballHitWall()                              ""
+    .walls barHitWall()               devuelve el wall, (enum walls)
+    .walls ballHitWall()                              ""
     .ballHitBlock()                 devuelve la pos del block o [-1, -1] si no le pego a nada
     .print_ball(ball_pos)
     .print_blocks(blocks)
     .print_bar(bar_pos)
+    .invertDirectionLR()            hace que la pelotita cambie de direccion si choca con una pared
+    .invertDirectionU()
 
+en el if 0=false; 1=true  SORRY TAMI SOY UN DESASTRE
 

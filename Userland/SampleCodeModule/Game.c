725bb5c26c3760651831ea19b6a55ec9d1556c2a
luciatorrusio <lucia.torrusio@gmail.com>
2019-11-06 21:36:09 -0300

    arrows cosas

--- Userland/SampleCodeModule/Game.c
+++ Userland/SampleCodeModule/Game.c
@@ -1,22 +1,23 @@
 #include "./include/Game.h"
 
-int XMIDDLE = SCREEN_WIDTH/2;           //la mitad en x de la panalla
-int YMIDDLE = SCREEN_HEIGHT/2;          //la mitad  en y de la pantalla
-int R_BLOCKS = 4;                       //Cantidad de filas de bloques
-int C_BLOCKS = 5;                       //Cantidad de columnas de bloques
-int LIVESi = 3;                         //cantidad de vidas al iniciar el juego
-enum ball_direc{LU, U, RU, RD, D, LD};  //Left up, up, right up, right down, down, left down
-enum walls{LEFT, RIGHT, UPPER, FLOOR};  //los bordes de la pantalla
-int bar_vel= 1;                         //velocidad de la barra 
+int XMIDDLE = SCREEN_WIDTH/2;               //la mitad en x de la panalla
+int YMIDDLE = SCREEN_HEIGHT/2;              //la mitad  en y de la pantalla
+int R_BLOCKS = 4;                           //Cantidad de filas de bloques
+int C_BLOCKS = 5;                           //Cantidad de columnas de bloques
+int LIVESi = 3;                             //cantidad de vidas al iniciar el juego
+enum ball_direc{LU, U, RU, RD, D, LD};      //Left up, up, right up, right down, down, left down
+enum walls{LEFT, RIGHT, UPPER, FLOOR};      //los bordes de la pantalla
+int bar_vel= 1;                             //velocidad de la barra 
 
 int runGame(void){
     
-    int lives = LIVESi;                      //cantidad de vidas que tiene
-    int blocks[R_BLOCKS][C_BLOCKS];          //matriz de los bloques
-    int ball_pos[]={XMIDDLE, YMIDDLE};       //pelota en el medio de ls pantalla
-    int ball_vel=1;                          //la velocidad cuenta de a cuantos cuadraditos se mueve
+    int lives = LIVESi;                     //cantidad de vidas que tiene
+    int blocks[R_BLOCKS][C_BLOCKS];         //matriz de los bloques
+    int ball_pos[]={XMIDDLE, YMIDDLE};      //pelota en el medio de ls pantalla
+    int ball_vel=1;                         //la velocidad cuenta de a cuantos cuadraditos se mueve
     int pos_bar = XMIDDLE;
-    barHitWall= false;
+    
+
     //pongo la matriz de bloques todos en uno, (osea que estan)
     for(int i = 0; i < C_BLOCKS ; i++){
         for(int j = 0; j < R_BLOCKS; j++){
@@ -49,9 +50,14 @@ int startGame(int bar_pos,int[R_BLOCKS][C_BLOCKS] blocks, int [2] ball_pos, int 
     print_bar(bar_pos);
 
 //barHitWall devuelve un int que representa que pared esta chocando (enum walls)
-    if(barHitWall()){
-        if (left_arrow_pressed() && Hit()==RIGHT){
-            bar_pos=bar_vel;
+    if(left_arrow_pressed()){
+       if(!barHitWall() || Hit() == RIGHT){
+             bar_pos  -= bar_vel;                     //la muevo para la izquierda
+       }
+    }
+    if(right_arrow_pressed()){
+        if(!barHitWall() || Hit() == LEFT){
+            bar_pos += bar_vel;
         }
     }
 
